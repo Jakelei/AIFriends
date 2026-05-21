534aabd4b869d4bd597300c09044ef97142f600d
lei chen <2585069414@qq.com>
2026-05-21 10:52:04 +0800

    实现前端语音输入

--- backend/web/views/friend/message/memory/update.py
+++ backend/web/views/friend/message/memory/update.py
@@ -1,4 +1,4 @@
-from pprint import pprint
+
 
 from django.utils.timezone import now
 from langchain_core.messages import SystemMessage, HumanMessage
@@ -36,10 +36,8 @@ def update_memory(friend):
         ]
     }
 
-    pprint(inputs)
     res = app.invoke(inputs)
     friend.memory = res['messages'][-1].content
-    pprint(friend.memory)
 
     friend.update_time = now()
     friend.save()
